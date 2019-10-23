3630a8a29e552665f5907043e5299853a4af45da
Kasper Knapik <kasper.knapik@gmail.com>
2019-10-23 18:34:19 +0100

    Initializing .NET Web App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZadaniePraca.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
